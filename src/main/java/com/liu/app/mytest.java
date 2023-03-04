40ae74898327afb5cb507c9f26b5e8c43fe1318f
lqw <2934187146@qq.com>
2023-03-04 16:40:17 +0800

    hot-fix one提交时

--- src/main/java/com/liu/app/mytest.java
+++ src/main/java/com/liu/app/mytest.java
@@ -4,5 +4,6 @@ public class mytest {
     public static void main(String[] args) {
         System.out.println("hello gitq213123");
         System.out.println("hahahaha");
+        System.out.println("my is hot-fix");
     }
 }

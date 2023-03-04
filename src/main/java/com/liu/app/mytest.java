ed14d8d3f6d90ffe7af8e004d4b62bb0837be3a2
lqw <2934187146@qq.com>
2023-03-04 17:33:08 +0800

    push

--- src/main/java/com/liu/app/mytest.java
+++ src/main/java/com/liu/app/mytest.java
@@ -7,5 +7,6 @@ public class mytest {
         System.out.println("my is hot-fix");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push");
     }
 }

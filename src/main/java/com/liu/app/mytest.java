229a19ced8f778c2004cb0bb005b5b249a2ccbcd
lqw-gitup-12 <126933623+lqw-gitup-12@users.noreply.github.com>
2023-03-04 18:09:25 +0800

    Update mytest.java

--- src/main/java/com/liu/app/mytest.java
+++ src/main/java/com/liu/app/mytest.java
@@ -8,5 +8,6 @@ public class mytest {
         System.out.println("master");
         System.out.println("hot-fix");
         System.out.println("push6666");
+        System.out.println("pull6666");
     }
 }
